15741c6d36b7a3fd84226c8e5a0fb99ca103c37e
minhnc1008 <97752763+minhiq200@users.noreply.github.com>
2023-05-25 19:37:17 +0700

    Update SuKienLichSu.java

--- Project/VietnamHistory/src/model/sukienlichsu/SuKienLichSu.java
+++ Project/VietnamHistory/src/model/sukienlichsu/SuKienLichSu.java
@@ -1,5 +1,77 @@
 package model.sukienlichsu;
 
-public class SuKienLichSu {
+import java.util.ArrayList;
+import model.thoiky.*;
+import model.nhanvatlichsu.*;
 
+public class SuKienLichSu {
+	
+	protected String ten;
+	protected String thoiGian;
+	protected String diaDiem;
+	protected ThoiKy thoiKy;
+	protected String noiDung;
+	protected ArrayList<NhanVatLichSu> nhanVatLienQuan;
+
+	public SuKienLichSu(String ten, String thoiGian, String diaDiem, ThoiKy thoiKy, String noiDung,
+			ArrayList<NhanVatLichSu> nhanVatLienQuan) {
+		super();
+		this.ten = ten;
+		this.thoiGian = thoiGian;
+		this.diaDiem = diaDiem;
+		this.thoiKy = thoiKy;
+		this.noiDung = noiDung;
+		this.nhanVatLienQuan = nhanVatLienQuan;
+	}
+	public SuKienLichSu(){
+	}
+
+	public String getTen() {
+		return ten;
+	}
+
+	public void setTen(String ten) {
+		this.ten = ten;
+	}
+
+	public String getThoiGian() {
+		return thoiGian;
+	}
+
+	public void setThoiGian(String thoiGian) {
+		this.thoiGian = thoiGian;
+	}
+
+	public String getDiaDiem() {
+		return diaDiem;
+	}
+
+	public void setDiaDiem(String diaDiem) {
+		this.diaDiem = diaDiem;
+	}
+
+	public ThoiKy getThoiKy() {
+		return thoiKy;
+	}
+
+	public void setThoiKy(ThoiKy thoiKy) {
+		this.thoiKy = thoiKy;
+	}
+
+	public String getNoiDung() {
+		return noiDung;
+	}
+
+	public void setNoiDung(String noiDung) {
+		this.noiDung = noiDung;
+	}
+
+	public ArrayList<NhanVatLichSu> getNhanVatLienQuan() {
+		return nhanVatLienQuan;
+	}
+
+	public void setNhanVatLienQuan(ArrayList<NhanVatLichSu> nhanVatLienQuan) {
+		this.nhanVatLienQuan = nhanVatLienQuan;
+	}
+	
 }
