ccd57fb99faed975484c6be56f959c4e20e9e96c
minhnc1008 <97752763+minhiq200@users.noreply.github.com>
2023-06-11 05:50:07 +0700

    Add files via upload

---
+++ Project/VietnamHistory/src/crawler/sukienlichsu/ChienTranhFull.java
@@ -0,0 +1,53 @@
+package crawler.sukienlichsu;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import crawler.util.dataout.IWriteJson;
+import model.sukienlichsu.*;
+import crawler.util.dataout.ICombine;
+public class ChienTranhFull implements ICombine, IWriteJson{
+	private ArrayList<ChienTranh> chienTranhFull;
+	private ArrayList<ChienTranh> chienTranhFull2;
+
+	private ChienTranh1 chientranh1;
+	private ChienTranh2 chientranh2;
+	
+	public ChienTranhFull(){
+		chienTranhFull = new ArrayList<ChienTranh>();
+	}
+	@Override
+	public void WriteJson() throws IOException {
+		// TODO Auto-generated method stub
+		String filePath = "src/data/sukienlichsu/ChienTranh.json";
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		try {
+			FileWriter writer = new FileWriter(new File(filePath));
+			gson.toJson(chienTranhFull, writer);
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void Combine() throws IOException {
+		// TODO Auto-generated method stub
+		chientranh1 = new ChienTranh1();
+		chientranh1.scraping();
+		chienTranhFull = chientranh1.getCTVN();
+		chientranh2 = new ChienTranh2();
+		chientranh2.scraping();
+		chienTranhFull2 = chientranh2.getChienTranh2();
+        
+		chienTranhFull.addAll(chienTranhFull2);
+
+	}
+	public static void main(String[] args) throws IOException {
+		ChienTranhFull t = new ChienTranhFull();
+		t.Combine();
+		t.WriteJson();
+	}
+}
