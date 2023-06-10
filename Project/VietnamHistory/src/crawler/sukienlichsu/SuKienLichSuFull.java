584d64eba21a70a0adc2d3b63f0812b9f8c3c4b3
minhnc1008 <97752763+minhiq200@users.noreply.github.com>
2023-06-11 05:49:39 +0700

    Update SuKienLichSuFull.java

--- Project/VietnamHistory/src/crawler/sukienlichsu/SuKienLichSuFull.java
+++ Project/VietnamHistory/src/crawler/sukienlichsu/SuKienLichSuFull.java
@@ -1,22 +1,55 @@
 package crawler.sukienlichsu;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
-import crawler.util.datain.IScraping;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import crawler.util.dataout.ICombine;
+import crawler.util.dataout.IWriteJson;
+import model.sukienlichsu.*;
+public class SuKienLichSuFull implements ICombine, IWriteJson {
+	private ArrayList<SuKienLichSu> suKienFull;
+	private SuKien1 sukien1;
+	private SuKien2 sukien2;
 
-public class SuKienLichSuFull implements ICombine, IScraping {
-
-	@Override
-	public void scraping() throws IOException {
-		// TODO Auto-generated method stub
-		
+	public SuKienLichSuFull(){
+		suKienFull = new ArrayList<SuKienLichSu>();
 	}
+
 
 	@Override
 	public void Combine() throws IOException {
 		// TODO Auto-generated method stub
-		
+		sukien1 = new SuKien1();
+		sukien1.scraping();
+		suKienFull = sukien1.getList();
+		sukien2 = new SuKien2();
+		sukien2.scraping();
+		suKienFull.addAll(sukien2.getSuKien2());
+
+	}
+
+	@Override
+	public void WriteJson() throws IOException {
+		// TODO Auto-generated method stub
+		String filePath = "src/data/sukienlichsu/SuKien.json";
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		try {
+			FileWriter writer = new FileWriter(new File(filePath));
+			gson.toJson(suKienFull, writer);
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		SuKienLichSuFull t = new SuKienLichSuFull();
+		t.Combine();
+		t.WriteJson();
 	}
 
 }
